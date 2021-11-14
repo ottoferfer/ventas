c80e554ab543f443c7c72c198afcc4fc2f71b553
ottoferfer <ottoferfer@gmail.com>
2021-11-14 02:25:38 -0600

    categorias

--- app/Http/Livewire/Categories.php
+++ app/Http/Livewire/Categories.php
@@ -4,12 +4,23 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
 
 class Categories extends Component
 {
+
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    public $pagination = 5;
+
     public function render()
     {
-        return view('livewire.category.categories')
+        $data = Category::all();
+        return view('livewire.category.categories', ['categories' => $data]);
         ->extends('layouts.theme.app')
         ->section('content');
     }
